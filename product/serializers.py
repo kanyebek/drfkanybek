489a3cb74d0567aceec7f032b269aa21e5fac685
zhoninho <zhanatbakirov05>
2025-04-23 19:51:36 +0600

    hw 2

--- product/serializers.py
+++ product/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
-from .models import *
+from .models import Category, Product, Review
 
-class ProductSerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
+    products_count = serializers.SerializerMethodField()
+
     class Meta:
-        model = Product
-        fields = '__all__'
+        model = Category
+        fields = ['id', 'name', 'products_count']
+
+    def get_products_count(self, category):
+            return Product.objects.filter(category=category).count()
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,8 +18,22 @@ class ReviewSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    products_count = serializers.SerializerMethodField()
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Category
+        model = Product
         fields = '__all__'
+
+
+class ProductWithReviewsSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+    rating = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'price', 'category', 'reviews', 'rating']
+
+    def get_rating(self, obj):
+        reviews = obj.reviews.all()
+        if reviews.exists():
+            return round(sum([r.stars for r in reviews]) / reviews.count(), 2)
+        return None
--- product/views.py
+++ product/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Category, Product, Review
-from .serializers import CategorySerializer, ProductSerializer, ReviewSerializer
+from .serializers import CategorySerializer, ProductSerializer, ReviewSerializer, ProductWithReviewsSerializer
 
 
 @api_view(['GET'])
@@ -53,4 +53,11 @@ def review_detail_api_view(request, id):
     except Review.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND, data={'error': 'Review does not exist!'})
     data = ReviewSerializer(review).data
+    return Response(data=data)
+
+
+@api_view(['GET'])
+def product_with_reviews_api_view(request):
+    products = Product.objects.all()
+    data = ProductWithReviewsSerializer(products, many=True).data
     return Response(data=data)
