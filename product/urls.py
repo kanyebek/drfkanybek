9d7eae4b250cee613548d2a0c591a23ff4152789
zhoninho <zhanatbakirov05>
2025-05-01 04:04:23 +0600

    hw 7

--- product/urls.py
+++ product/urls.py
@@ -9,10 +9,9 @@ from .views import (
 )
 
 urlpatterns = [
+    path('', ProductListCreateAPIView.as_view()),
+    path('<int:id>/', ProductDetailAPIView.as_view()),
     path('categories/', CategoryListCreateAPIView.as_view()),
     path('categories/<int:id>/', CategoryDetailAPIView.as_view()),
-
-    path('products/', ProductListCreateAPIView.as_view()),
-    path('products/<int:id>/', ProductDetailAPIView.as_view()),
-    path('products/reviews/', ProductWithReviewsAPIView.as_view()),
+    path('reviews/', ProductWithReviewsAPIView.as_view()),
 ]
--- shop_api/settings.py
+++ shop_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8$z(mq43_2s0n12(-@e4)ez5*%a-0f3b**tpipwqj!-kcbxv#5'
+SECRET_KEY = os.environ.get('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == 'on' else False
 
 ALLOWED_HOSTS = []
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'product',
     'users',
     'rest_framework.authtoken',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -100,8 +101,12 @@ WSGI_APPLICATION = 'shop_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
